029a9ec5e613c036248e52e07969ee83e98c8468
Ragamn <ri.takahashi.sys22@morijyobi.ac.jp>
2024-12-13 14:08:26 +0900

    画像の表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,41 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-customtitle">RIKI TAKAHASHI</h1>
-      <p className="text-customtitle">ENGINIEER PORTFOLIO</p>
-      <h2 className="text-customtitle">ABOUT ME</h2>
-      <h2 className="text-customtitle">SKILL</h2>
-      <h2 className="text-customtitle">PORTFOLIO</h2>
+      <div>
+        <h1 className="text-customtitle">RIKI TAKAHASHI</h1>
+        <p className="text-customtitle">ENGINIEER PORTFOLIO</p>
+      </div>
+      <div>
+        <h2 className="text-customtitle">ABOUT ME</h2>
+        <Image
+          src="/image/dami.png"
+          alt="Dami Image"
+          width={650}
+          height={390}
+        />
+      </div>
+      <div>
+        <h2 className="text-customtitle">SKILL</h2>
+        <Image
+          src="/image/dami.png"
+          alt="Dami Image"
+          width={650}
+          height={390}
+        />
+      </div>
+      <div>
+        <h2 className="text-customtitle">PORTFOLIO</h2>
+        <Image
+          src="/image/dami.png"
+          alt="Dami Image"
+          width={650}
+          height={390}
+        />
+      </div>
+
+      <img></img>
     </div>
   );
 }
