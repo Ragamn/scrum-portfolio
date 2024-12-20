fa3e8a3f83305b7c7034b8d4c076fea57a01797b
Ragamn <ri.takahashi.sys22@morijyobi.ac.jp>
2024-12-20 10:16:53 +0900

    メニューバーの読み込み

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./style/globals.css";
+import MenuBar from "./components/menu-bar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-customBackground`}
       >
+        <MenuBar />
         {children}
       </body>
     </html>
